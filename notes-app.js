290cfc76421d3e75c777c08c8e67a7b3ce70ab14
Ankush Dev <Ankush Dev@MCKINSEY.COM>
2018-09-02 22:05:29 +0200

    adding localStorage for persistance

--- notes-app.js
+++ notes-app.js
@@ -1,17 +1,25 @@
-const notes =[ {title: 'My next trip',
-                body: 'I would like to go to Spain'
-                },
-                {title: 'Habits to work on',
-                body: 'Exercise, eating a bit better'
-                },
-                {title: 'Office modifications',
-                body: 'Get a new seat' 
-                }
-            ]
+// const notes =[ {title: 'My next trip',
+//                 body: 'I would like to go to Spain'
+//                 },
+//                 {title: 'Habits to work on',
+//                 body: 'Exercise, eating a bit better'
+//                 },
+//                 {title: 'Office modifications',
+//                 body: 'Get a new seat' 
+//                 }
+//             ]
+
+let notes = []
 
 const filters = {
     searchText: ''
 }            
+
+//Check for existing saved data.
+const notesJSON = localStorage.getItem('notes')
+if (notesJSON !== null){
+    notes = JSON.parse(notesJSON)
+}
 
 //Render notes based on provided filters. First time render all notes as filters is empty.
 //Usage of includes String method.
@@ -25,7 +33,15 @@ const renderNotes = function(notes, filters){
 
     filteredNotes.forEach(function(note){
         const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
+
+        if (note.title.length > 0) {
+            noteEl.textContent = note.title
+        }
+        else {
+            noteEl.textContent = 'Unnamed note'
+        }
+
+        
         document.querySelector('#notes').appendChild(noteEl)
     })
 }
@@ -34,7 +50,13 @@ const renderNotes = function(notes, filters){
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click',function(e){
-    console.log('The button was clicked')
+    // console.log('The button was clicked')
+    notes.push({
+        title: '',
+        body: ''
+    })
+    localStorage.setItem('notes', JSON.stringify(notes))
+    renderNotes(notes, filters)
 })
 
 document.querySelector('#search-text').addEventListener('input', function(e){
